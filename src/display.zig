1bbf9ed61c98bf8d4ba774202920fe7adb64544d
Rohan Vashisht <rohanprogrammer1@gmail.com>
2025-10-18 06:07:25 +0530

    added a lot of colours

--- src/display.zig
+++ src/display.zig
@@ -1,47 +1,52 @@
 const std = @import("std");
+const ansi = @import("./libs/ansi_codes.zig");
 
 pub const help = struct {
     pub fn all_info() void {
-        const help_message =
-            \\Welcome to Zigistry-cli!
-            \\ 
-            \\ 
-            \\Here are some commands we have
-            \\------------------------------------------------
-            \\install - This is used to install a Zig package.
-            \\Example Usage:
-            \\        zigp install capy-ui/capy
-            \\------------------------------------------------
-            \\version - Displays the version of zigp.
-            \\------------------------------------------------
-            \\self-update - Zigp will update itself.
-            \\------------------------------------------------
-            \\
-            \\♥ Made with Love by https://zigistry.dev ♥
-            \\
-        ;
-        std.debug.print(help_message, .{});
+        std.debug.print("{s}╔══════════════════════════════════════════════════════╗{s}\n", .{ ansi.BRIGHT_CYAN, ansi.RESET });
+        std.debug.print("{s}║               Welcome to Zigistry-CLI!               ║{s}\n", .{ ansi.BRIGHT_CYAN, ansi.RESET });
+        std.debug.print("{s}╚══════════════════════════════════════════════════════╝{s}\n\n", .{ ansi.BRIGHT_CYAN, ansi.RESET });
+
+        std.debug.print("{s}Here are some commands you can use:{s}\n", .{ ansi.BRIGHT_YELLOW, ansi.RESET });
+        std.debug.print("{s}--------------------------------------------------------{s}\n", .{ ansi.BRIGHT_WHITE, ansi.RESET });
+
+        std.debug.print("{s}install{s}      - Install a Zig package.\n", .{ ansi.BRIGHT_GREEN, ansi.RESET });
+        std.debug.print("    Example: {s}zigp{s} install {s}capy-ui/capy{s}\n", .{ ansi.BRIGHT_MAGENTA, ansi.BRIGHT_WHITE, ansi.BRIGHT_MAGENTA, ansi.RESET });
+        std.debug.print("{s}--------------------------------------------------------{s}\n", .{ ansi.BRIGHT_WHITE, ansi.RESET });
+
+        std.debug.print("{s}version{s}      - Display the version of zigp.\n", .{ ansi.BRIGHT_GREEN, ansi.RESET });
+        std.debug.print("{s}--------------------------------------------------------{s}\n", .{ ansi.BRIGHT_WHITE, ansi.RESET });
+
+        std.debug.print("{s}self-update{s}  - Update zigp itself to the latest version.\n", .{ ansi.BRIGHT_GREEN, ansi.RESET });
+        std.debug.print("{s}--------------------------------------------------------{s}\n\n", .{ ansi.BRIGHT_WHITE, ansi.RESET });
+
+        std.debug.print("{s}♥{s} Made with {s}Love{s} by {s}https://zigistry.dev{s} {s}♥{s}\n\n", .{ ansi.BRIGHT_MAGENTA, ansi.RESET, ansi.BRIGHT_YELLOW, ansi.RESET, ansi.BRIGHT_CYAN, ansi.RESET, ansi.BRIGHT_MAGENTA, ansi.RESET });
     }
+
     pub fn install_info() void {
-        const help_message =
-            \\The install command is used to install a package into a zig project.
-            \\
-            \\Syntax:
-            \\         zigp install <package-name>
-            \\Example usage:
-            \\         zigp install capy-ui/capy
-            \\
-        ;
-        std.debug.print(help_message, .{});
+        std.debug.print("{s}╔══════════════════════════════════════════════╗\n", .{
+            ansi.BRIGHT_CYAN,
+        });
+        std.debug.print("║            Zigistry Install Command          ║\n", .{});
+        std.debug.print("╚══════════════════════════════════════════════╝{s}\n\n", .{ansi.RESET});
+
+        std.debug.print("{s}Description:{s}\n", .{ ansi.BRIGHT_YELLOW, ansi.RESET });
+        std.debug.print("    The install command is used to add a package into your Zig project.\n\n", .{});
+
+        std.debug.print("{s}Syntax:{s}\n", .{ ansi.BRIGHT_YELLOW, ansi.RESET });
+        std.debug.print("    {s}zigp{s} install {s}<package-name>{s}\n\n", .{ ansi.BRIGHT_MAGENTA, ansi.BRIGHT_GREEN, ansi.BRIGHT_MAGENTA, ansi.RESET });
+
+        std.debug.print("{s}Example:{s}\n", .{ ansi.BRIGHT_YELLOW, ansi.RESET });
+        std.debug.print("    {s}zigp{s} install {s}capy-ui/capy{s}\n\n", .{ ansi.BRIGHT_MAGENTA, ansi.BRIGHT_GREEN, ansi.BRIGHT_MAGENTA, ansi.RESET });
     }
 };
 
 pub const err = struct {
     pub fn unknown_argument(x: []const u8) void {
-        std.debug.print("Unknown argument recieved: {s}\n", .{x});
+        std.debug.print("{s}{s}Unknown argument recieved: {s}{s}\n", .{ ansi.RED, ansi.BOLD, x, ansi.RESET });
     }
     pub fn failed_self_update() void {
-        std.debug.print("Zigp wasn't able to update itself. Some error occured in the build script.\n", .{});
+        std.debug.print("{s}{s}Zigp wasn't able to update itself. Some error occured in the build script.{s}\n", .{ ansi.RED, ansi.BOLD, ansi.RESET });
     }
     pub fn unexpected_failed_self_update(x: u8) void {
         std.debug.print("Zigp wasn't able to update itself due to an unknown or unexpected issue. Value returned: {}\n", .{x});
--- src/install.zig
+++ src/install.zig
@@ -1,8 +1,9 @@
 const std = @import("std");
 const MAX_ALLOWED_REPO_NAME_LENGTH = 2000;
 const url = "https://api.github.com/repos/{s}/releases";
+const ansi = @import("./libs/ansi_codes.zig");
 
-pub fn fetch_versions(repo: []const u8) !std.StringArrayHashMap([]const u8) {
+pub fn fetch_versions(repo: []const u8) !std.ArrayList([]const u8) {
     // I am doing -2 for making sure {} is not included.
     if (url.len + repo.len - 2 > MAX_ALLOWED_REPO_NAME_LENGTH) {
         @panic("The length of repo name is way too much long.");
@@ -15,27 +16,44 @@ pub fn fetch_versions(repo: []const u8) !std.StringArrayHashMap([]const u8) {
     var response = std.Io.Writer.Allocating.init(std.heap.c_allocator);
     defer response.deinit();
     const result = try client.fetch(.{ .location = .{ .url = fetch_url }, .response_writer = &response.writer });
-    if (result.status != .ok) @panic("oh no");
+    if (result.status == .not_found) {
+        std.debug.print("{s}Error: {s}{s}{s} is not a repo.\n", .{ ansi.RED ++ ansi.BOLD, ansi.BRIGHT_CYAN, repo, ansi.RESET });
+        std.process.exit(0);
+    }
+    if (result.status != .ok) {
+        std.debug.print("{s}Didn't recieve a responce, please check your internet connection.{s}", .{ ansi.RED ++ ansi.BOLD, ansi.RESET });
+    }
+    std.debug.print("{s}Installing {s}{s}.{s}\n", .{ ansi.YELLOW, ansi.UNDERLINE, repo, ansi.RESET });
+    std.debug.print("{s}Please select the version you want to install:{s}\n", .{ ansi.BRIGHT_CYAN ++ ansi.BOLD, ansi.RESET });
     const body = response.written();
+    if (body.len == 0 or body[0] != '[') {
+        std.debug.print("{s}A non json responce was recieved which is most likely an error, responce recieved:\n{s}", .{ ansi.RED ++ ansi.BOLD, ansi.RESET });
+        std.debug.print("{s}\n", .{body});
+        std.process.exit(0);
+    }
 
     const j: std.json.Parsed(std.json.Value) = try std.json.parseFromSlice(std.json.Value, std.heap.c_allocator, body, .{});
-    var hashmap = std.StringArrayHashMap([]const u8).init(std.heap.page_allocator);
+    var list: std.ArrayList([]const u8) = .empty;
+    try list.append(std.heap.c_allocator, "Master Branch (unstable)");
     const all_releases = j.value.array.items;
     if (all_releases.len != 0) {
         for (all_releases) |single_release| {
-            try hashmap.put(single_release.object.get("tag_name").?.string, single_release.object.get("tarball_url").?.string);
+            try list.append(std.heap.c_allocator, single_release.object.get("tag_name").?.string);
         }
-        return hashmap;
+        return list;
     }
     unreachable;
 }
 
-pub fn install_package(x: []const u8) !void {
-    std.debug.print("Installing {s}.\n", .{x});
-    std.debug.print("Please select the version you want to install:\n", .{});
-    const d = try fetch_versions(x);
-    var iter = d.iterator();
-    while (iter.next()) |next_value| {
-        std.debug.print("{s}:{s}\n", .{next_value.key_ptr.*, next_value.value_ptr.*});
+// https://github.com/RohanVashisht1234/zorsig/archive/refs/tags/v0.0.1.tar.gz
+// https://github.com/{}/archive/refs/tags/{}.tar.gz
+
+pub fn install_package(repo_name: []const u8) !void {
+    const d = try fetch_versions(repo_name);
+    // const x = "https://github.com/{}/archive/refs/tags/{}.tar.gz";
+    for (d.items, 1..) |value, i| {
+        std.debug.print("{}){s} {s}{s}\n", .{ i, ansi.BOLD, value, ansi.RESET });
     }
+
+    
 }
