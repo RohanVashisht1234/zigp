bb714cfecb13b16aeacd2534664e8dd648a08a46
Rohan Vashisht <rohanprogrammer1@gmail.com>
2025-10-23 06:02:39 +0530

    added readme

---
+++ README.md
@@ -0,0 +1,16 @@
+# Experimental Zig CLI package manager
+
+To install run:
+
+```bash
+curl -fsSL https://raw.githubusercontent.com/RohanVashisht1234/zigp/refs/heads/main/install_script.sh | bash
+```
+
+In the directory of your zig project, run in your terminal:
+
+```bash
+zigp add gh/RohanVashisht1234/zorsig
+
+# Example:
+zigp add gh/capy-ui/capy
+```
--- install_script.sh
+++ install_script.sh
@@ -1,15 +1,15 @@
 TMP_DIR=$(mktemp -d)
 echo "Created: $TMP_DIR"
 cd "$TMP_DIR" || exit 1
-git clone https://github.com/Zigistry/zigp --depth=1
+git clone https://github.com/rohanvashisht1234/zigp --depth=1
 cd zigp
 echo "Installing zigp: $TMP_DIR"
-zig build install --prefix ~/.local
+zig build install --prefix ~/.local/zigp
 
-if [[ ":$PATH:" != *":$HOME/.local/bin:"* ]]; then
-  echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc 2>/dev/null || true
-  echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.zshrc 2>/dev/null || true
-  echo "Added ~/.local/bin to your PATH (will apply on next shell start)"
+if [[ ":$PATH:" != *":$HOME/.local/zigp/bin:"* ]]; then
+  echo 'export PATH="$HOME/.local/zigp/bin:$PATH"' >> ~/.bashrc 2>/dev/null || true
+  echo 'export PATH="$HOME/.local/zigp/bin:$PATH"' >> ~/.zshrc 2>/dev/null || true
+  echo "Added ~/.local/bin/zigp to your PATH (will apply on next shell start)"
 fi
 
 rm -rf "$TMP_DIR"
